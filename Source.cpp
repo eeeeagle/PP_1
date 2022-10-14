8752ecb1a2e767fb6291b7fca01dc314542c2f73
Daniil Osokin <daniil.osockin@gmail.com>
2022-10-15 01:32:08 +0400

    Update

--- Source.cpp
+++ Source.cpp
@@ -1,12 +1,45 @@
 #include <iostream>
-//#include <fstream>
+#include <fstream>
 #include <string>
 #include <vector>
+
+typedef std::vector<std::vector<int>> Matrix;
 
 bool is_exists(const std::string& name) 
 {
 	struct stat buffer;
 	return (stat(name.c_str(), &buffer) == 0);
+}
+
+Matrix multiply_matrix(const Matrix& a, const Matrix& b)
+{
+	Matrix c;
+
+	/* Допилить */
+
+	return c;
+}
+
+int read_matrix(Matrix& matrix, const std::string& filepath)
+{
+	std::ifstream file(filepath);
+	if (!file)
+		return -1;
+
+	/* допилить */
+
+	file.close();
+	return 0;
+}
+
+int write_matrix(	const Matrix&		matrix,
+					const unsigned&		runtime, 
+					const unsigned&		volume,
+					const std::string&	filepath = "%USERPROFILE%/Documents/output.txt")
+{
+	/* допилить */
+
+	return 0;
 }
 
 int main(int argc, char** argv)
@@ -33,8 +66,17 @@ int main(int argc, char** argv)
 		exit(EXIT_FAILURE);
 	}
 
-	std::vector<std::vector<int>> matrix_1, matrix_2;
-	
+	Matrix a, b;
+	read_matrix(a, argv[1]);
+	read_matrix(b, argv[2]);
+
+	unsigned volume = 0; /* ??? */
+
+	clock_t start_time = clock();
+	Matrix с = multiply_matrix(a, b);
+	clock_t end_time = clock();
+
+	write_matrix(с, end_time - start_time, volume);
 
 	return 0;
 }
